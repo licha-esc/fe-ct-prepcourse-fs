d90bc3ff72bc2fb2c72fe3c8e4eac515a3499a4c
Licha <104387269+licha-esc@users.noreply.github.com>
2024-02-04 10:08:19 -0300

    m08

--- M08 Objects + Callbacks/objects/ejercicios/03.js
+++ M08 Objects + Callbacks/objects/ejercicios/03.js
@@ -1,6 +1,8 @@
 const agregarNuevaPropiedad = (objeto, propiedad, valor) => {
   // Añade una nueva propiedad al objeto con su respectivo valor pasado por la función.
   // Tu código:
+  const nuevoObjeto = { ...objeto, [propiedad]: valor };
+  return nuevoObjeto;
 };
 
 module.exports = agregarNuevaPropiedad;
